8847e7a3fd34b07d4c14879ff1f4f67b2059a111
Drew Rautenberg <drew@drewrautenberg.com>
2017-09-27 16:03:00 -0400

    formatted code

--- src/DoEWithFor.java
+++ src/DoEWithFor.java
@@ -1,5 +1,3 @@
-import javax.swing.*;
-
 /* PROJECT:  DoEWithFor       (draw a big E using FOR loops)
  * AUTHOR: ___________________________      DESIGNER:  Dr. Kaminski
  * DESCRIPTION:  Programs draws a big letter E (in console window) based on
@@ -13,6 +11,9 @@ import javax.swing.*;
  *      - the remainder of HEIGHT is used by the 2 verticals, with the top one
  *          being slightly smaller, if that remainder is an odd number
  *****************************************************************************/
+
+import javax.swing.*;
+
 public class DoEWithFor {
 
     public static void main(String[] args) {
@@ -27,27 +28,30 @@ public class DoEWithFor {
         int topVertHeight;
         int bottomVertHeight;
         int midWidth;
+        int sudoHeight;
         // -------------------------------------------------------------- INPUT
         // TEMPORARY HARD-CODED "input"
-        height = 14;
-        width = 15;
-        pen = 'X';
-        thickness = 2;
+        //height = 14;
+        //width = 15;
+        //pen = 'X';
+        //thickness = 2;
 
-/*
+
         height = Integer.parseInt(JOptionPane.showInputDialog("Enter Height"));
         width = Integer.parseInt(JOptionPane.showInputDialog("Enter Width"));
         pen = JOptionPane.showInputDialog("Character to print with").charAt(0);
-*/
+        thickness = Integer.parseInt(JOptionPane.showInputDialog("Enter Thickness"));
+
+
         //calculate top vertical height
-        topVertHeight = (height - 6) / 2;
+        //height= sudoHeight*2;
+        topVertHeight = (height - (3 * thickness)) / 2;
 
         //calculate bottom vertical height
-        if (height % 2 == 0){
+        if (height % 2 == 0) {
+            bottomVertHeight = topVertHeight + 1;
+        } else {
             bottomVertHeight = topVertHeight;
-        }
-        else {
-            bottomVertHeight = topVertHeight+1;
         }
         //calculate middle horizontal length
         midWidth = width / 2;
@@ -63,33 +67,33 @@ public class DoEWithFor {
         //          function of width, height, thickness
         //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
         // 1) TOP HORIZONTAL
-        for (i = 0; i < 4; i++) {
+        for (i = 0; i < thickness * 2; i++) {
             for (j = 0; j < width; j++) {
                 System.out.print(pen);
             }
             System.out.println();
         }
         // 2) TOP VERTICAL
-        for (i = 0; i < topVertHeight * 2; i++){
+        for (i = 0; i < topVertHeight * thickness * 2; i++) {
             System.out.print(pen);
             System.out.print(pen);
             System.out.println();
         }
         // 3) MIDDLE HORIZONTAL
-        for (i = 0; i < 4; i++) {
+        for (i = 0; i < 2 * thickness; i++) {
             for (j = 0; j < midWidth; j++) {
                 System.out.print(pen);
             }
             System.out.println();
         }
         // 4) BOTTOM VERTICAL
-        for (i = 0; i < bottomVertHeight * 2; i++){
+        for (i = 0; i < bottomVertHeight * thickness * 2; i++) {
             System.out.print(pen);
             System.out.print(pen);
             System.out.println();
         }
         // 5) BOTTOM HORIZONTAL
-        for (i = 0; i < 4; i++) {
+        for (i = 0; i < thickness * 2; i++) {
             for (j = 0; j < width; j++) {
                 System.out.print(pen);
             }
