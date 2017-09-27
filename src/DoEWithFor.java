5e9f8c2a276c46a8e4f9ebea5bc5ac3a6d2dc276
Drew Rautenberg <drew@drewrautenberg.com>
2017-09-27 08:43:46 -0400

    Initial commit

---
+++ src/DoEWithFor.java
@@ -0,0 +1,55 @@
+/* PROJECT:  DoEWithFor       (draw a big E using FOR loops)
+ * AUTHOR: ___________________________      DESIGNER:  Dr. Kaminski
+ * DESCRIPTION:  Programs draws a big letter E (in console window) based on
+ *      user-supplied parameters (dialog boxes):
+ *              drawingChar, penThickness, charHeight, charWidth.
+ * RULES FOR E's FONT - see comment in code below
+ * ASSUMPTION for drawing in Console window: - it prints from top to bottom,
+ *      and, for a particular row, it prints from left to right
+ * THINKING about the size of the 2 verticals. . .
+ *      - part of the entire char's HEIGHT is taken up by the 3 horizontals
+ *      - the remainder of HEIGHT is used by the 2 verticals, with the top one
+ *          being slightly smaller, if that remainder is an odd number
+ *****************************************************************************/
+public class DoEWithFor {
+
+    public static void main(String[] args) {
+        // ---------------------------------------------------------- VARIABLES
+        String userInput;
+        int height;
+        int width;
+        char pen;
+        int thickness;
+        // -------------------------------------------------------------- INPUT
+        // TEMPORARY HARD-CODED "input"
+        height = 14;
+        width = 15;
+        pen = 'X';
+        thickness = 2;
+        // --------------------------------------------------- DRAW THE 5 LINES
+        // RULES FOR E's
+        //      - all lines are 2 X's thick
+        //      - top & bottom horizontals are both full width
+        //      - middle horizontal is 1/2 width
+        //      - top & bottom verticals are same size when height is odd,
+        //          but for even heights, bottom vertical is slightly taller
+        //      - things below shouldn't be hardcoded, but calculated as a
+        //          function of width, height, thickness
+        //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        // 1) TOP HORIZONTAL
+
+
+        // 2) TOP VERTICAL
+
+
+        // 3) MIDDLE HORIZONTAL
+
+
+        // 4) BOTTOM VERTICAL
+
+
+        // 5) BOTTOM HORIZONTAL
+
+
+    }
+}
